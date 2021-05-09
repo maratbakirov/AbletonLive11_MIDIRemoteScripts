8a5e6ea7cb481c155b27b21724695173e3708ade
mbakirov <mbakirov@hotmail.com>
2021-05-09 17:26:10 +0800

    Original version and a solution added.

---
+++ Novation_Impulse2/deploy.bat
@@ -0,0 +1,2 @@
+del /Q "C:\ProgramData\Ableton\Live 11 Suite\Resources\MIDI Remote Scripts\Novation_Impulse2\*.*"
+xcopy *.py "C:\ProgramData\Ableton\Live 11 Suite\Resources\MIDI Remote Scripts\Novation_Impulse2\*.*"
